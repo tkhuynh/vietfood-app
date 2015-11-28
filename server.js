d74222a4decceb4bf552ef166a45c59424020adb
tkhuynh <thequest2311@yahoo.com>
2015-11-28 14:43:44 -0800

    setup get request for different dishes using Yelp API, hid all keys

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var express = require("express"),
     cookieParser = require("cookie-parser"),
     session = require("express-session"),
     passport = require("passport"),
-    LocalStrategy = require("passport-local").Strategy,
-    Yelp = require("yelp");
+    LocalStrategy = require("passport-local").Strategy;
+
+require('dotenv').load();
+var Yelp = require("yelp");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,7 +21,29 @@ hbs.registerPartials(__dirname + "/views/partials");
 
 //hello word test
 app.get("/", function (req, res) {
-	res.send("hello world");
+	res.render("index");
+});
+
+//import yelp API needed keys
+var yelp = new Yelp({
+  consumer_key: process.env.my_consumer_key,
+  consumer_secret: process.env.my_consumer_secret,
+  token: process.env.my_token,
+  token_secret: process.env.my_token_secret
+});
+//different get request for different disdes
+var dishes = ["banh mi", "bun bo hue", "pho", "bun thit nuong", "bo luc lac", "cha gio", "goi cuon", "cafe sua da", "bun rieu", "che ba mau", "goi du du", "com tam bi suon cha", "hu tieu", "bo kho", "banh xeo"];
+
+dishes.forEach(function (dish) {
+	app.get("/api/" + dish.replace(/\s/g,""), function (req, res) {
+		yelp.search({ term: dish, location: "San Francisco, CA", sort: 0, category_filter: "vietnamese", radius_filter: 10000, limit: 10})
+			.then(function (data) {
+				res.json({restaurants : data.businesses});
+			})
+				.catch(function (err) {
+					console.error(err);
+				});
+	});
 });
 
 //listen to port 3000
