f987e67c381b66babeb17908d8abbed3964f3404
tkhuynh <thequest2311@yahoo.com>
2015-11-28 15:07:25 -0800

    setup get request on client side, user choose a dish, show restaurant sell it on map

---
+++ public/main.js
@@ -0,0 +1,51 @@
+$(function() {
+	var createMap = function() {
+    	map = new google.maps.Map(document.getElementById('map'), {
+      	center: { lat: 37.78, lng: -122.44 },
+      	zoom: 13
+    	});
+  	};
+
+  var $description = $("#foodDescription");
+  var source = $("#description-template").html();
+  var template = Handlebars.compile(source);
+
+  $(".dropdown-menu li").click(function (event) {
+  	var keyword = $(this).text();
+  	$("span[data-bind='label']").html(keyword);
+		keyword = keyword.toLowerCase();
+		var dish = keyword.replace(/\s/g, "");
+		$.get("/api/" + dish, function (data) {
+			console.log(data);
+			var restaurants = data.restaurants;
+			restaurants.forEach(function(restaurant) {
+				// $("#result").append("<h3>"+restaurant.name+"</h3>");
+				var contentString = '<div id="content">'+
+											      '<div id="siteNo(tice">'+
+											      '</div>'+
+											      '<h4 id="firstHeading" class="firstHeading">'+ restaurant.name+'</h4>'+
+											      '<div id="bodyContent">'+
+											      '<p>'+restaurant.location.display_address[0]+
+											      ', '+restaurant.location.display_address[1]+
+											      '<br>'+restaurant.location.display_address[2]+'</p>'+
+											      '</div>'+
+											      '</div>';
+				var infowindow = new google.maps.InfoWindow({
+					content: contentString
+				});
+				var latitude = restaurant.location.coordinate.latitude;
+				var longitude = restaurant.location.coordinate.longitude;
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(latitude, longitude),
+					map: map,
+					title: restaurant.name,
+					icon: 'restaurant.png' 
+				});
+				marker.addListener("click", function () {
+					infowindow.open(map, marker);
+				});
+			});
+		});
+		createMap();
+	});
+});
