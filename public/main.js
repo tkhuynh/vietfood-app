e4b53bd19f42ecf7641f954017f18da1c3a83ce3
tkhuynh <thequest2311@yahoo.com>
2015-11-28 19:24:39 -0800

    finish Top Dishes option on mainpage

--- public/main.js
+++ public/main.js
@@ -14,62 +14,70 @@ $(function() {
   var source2 = $("#restaurant-template").html();
   var template2 = Handlebars.compile(source2);
 
-  $(".dropdown-menu li").click(function (event) {
-  	var keyword = $(this).text();
-  	$("span[data-bind='label']").html(keyword);
-		keyword = keyword.toLowerCase();
-		var dish = keyword.replace(/\s/g, "");
-		$.get("/api/" + dish, function (data) {
-			var restaurants = data.restaurants;
-			var leftResult = restaurants.slice(0,5);
-			var rightResult = restaurants.slice(-5);
-			$("#restaurantList").empty();
-			var restaurantHtmlLeft = template2({lefts: leftResult});
-			$("#restaurantList").append(restaurantHtmlLeft);
-			var restaurantHtmlRight = template2({rights: rightResult});
-			$("#restaurantList").append(restaurantHtmlRight);
+  //Top Dishes Option chosen
+ 	$("#top").click(function () {
+ 		$("#commonDishes").show();
+	  $(".dropdown-menu li").click(function (event) {
+	  	var keyword = $(this).text();
+	  	$("span[data-bind='label']").html(keyword);
+			keyword = keyword.toLowerCase();
+			var dish = keyword.replace(/\s/g, "");
+			function restaurantsSellThisDish () {
+				$.get("/api/" + dish, function (data) {
+					var restaurants = data.restaurants;
+					var leftResult = restaurants.slice(0,5);
+					var rightResult = restaurants.slice(-5);
+					$("#restaurantList").empty();
+					var restaurantHtmlLeft = template2({lefts: leftResult});
+					$("#restaurantList").append(restaurantHtmlLeft);
+					var restaurantHtmlRight = template2({rights: rightResult});
+					$("#restaurantList").append(restaurantHtmlRight);
 
-			restaurants.forEach(function(restaurant) {
-				var contentString = '<div id="content">'+
-											      '<div id="siteNo(tice">'+
-											      '</div>'+
-											      '<h4 id="firstHeading" class="firstHeading">'+ restaurant.name+'</h4>'+
-											      '<div id="bodyContent">'+
-											      '<p>'+restaurant.location.display_address[0]+
-											      ', '+restaurant.location.display_address[1]+
-											      '<br>'+restaurant.location.display_address[2]+'</p>'+
-											      '</div>'+
-											      '</div>';
-				var infowindow = new google.maps.InfoWindow({
-					content: contentString
+					restaurants.forEach(function(restaurant) {
+						var contentString = '<div id="content">'+
+													      '<div id="siteNo(tice">'+
+													      '</div>'+
+													      '<h4 id="firstHeading" class="firstHeading">'+ restaurant.name+'</h4>'+
+													      '<div id="bodyContent">'+
+													      '<p>'+restaurant.location.display_address[0]+
+													      ', '+restaurant.location.display_address[1]+
+													      '<br>'+restaurant.location.display_address[2]+'</p>'+
+													      '</div>'+
+													      '</div>';
+						var infowindow = new google.maps.InfoWindow({
+							content: contentString
+						});
+						var latitude = restaurant.location.coordinate.latitude;
+						var longitude = restaurant.location.coordinate.longitude;
+						var marker = new google.maps.Marker({
+							position: new google.maps.LatLng(latitude, longitude),
+							map: map,
+							title: restaurant.name,
+							icon: 'restaurant.png' 
+						});
+						marker.addListener("click", function () {
+							infowindow.open(map, marker);
+						});
+					});
 				});
-				var latitude = restaurant.location.coordinate.latitude;
-				var longitude = restaurant.location.coordinate.longitude;
-				var marker = new google.maps.Marker({
-					position: new google.maps.LatLng(latitude, longitude),
-					map: map,
-					title: restaurant.name,
-					icon: 'restaurant.png' 
+				
+				//get description of dish from manually built api then append to the page
+				$.get("/api/description", function (data) {
+					var dishes = data.dishes;
+					var foundDish;
+					dishes.forEach(function (dish) {
+						if (dish.name.toLowerCase() == keyword) {
+							foundDish = dish;
+						}
+					});
+					$("#foodDescription").empty();
+					var descriptionHtml = template1({dish: foundDish});
+					$description.append(descriptionHtml);
 				});
-				marker.addListener("click", function () {
-					infowindow.open(map, marker);
-				});
-			});
+				createMap();
+			}
+			restaurantsSellThisDish();
 		});
-		
-		//get description of dish from manually built api then append to the page
-		$.get("/api/description", function (data) {
-			var dishes = data.dishes;
-			var foundDish;
-			dishes.forEach(function (dish) {
-				if (dish.name.toLowerCase() == keyword) {
-					foundDish = dish;
-				}
-			});
-			$("#foodDescription").empty();
-			var descriptionHtml = template1({dish: foundDish});
-			$description.append(descriptionHtml);
-		});
-		createMap();
 	});
+
 });
